9701a3af2023fe805dd7c4814060ad8d3dd50e1c
emalianakasmuri <emaliana.kasmuri@gmail.com>
2024-10-04 09:02:18 +0800

    Update documentation style

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
-/**
- * Purpose:
- * This app display a set of text using a column.
- */
 
-/**
- * Main entry poin to the app
- */
+/// BITP 3453 Mobile Application Development
+/// Lecture 02
+///
+/// Purpose:
+/// This app display a set of text using a column.
+
+
+/// Main entry point to the app
 void main() {
 
   // Instantiate and run the app
-  runApp(ColumnApp01());
+  runApp(const ColumnApp01());
 }
 
-/**
- * Class that represents the app
- */
+/// Class that represents the app
 class ColumnApp01 extends StatelessWidget {
+  const ColumnApp01({super.key});
 
-  /**
-   * This method creates the UI
-   */
+  /// This method creates the UI
   @override
   Widget build(BuildContext context) {
 
@@ -29,31 +27,44 @@ class ColumnApp01 extends StatelessWidget {
       // Scaffold: lay the UI widget on screen
       home: Scaffold (
 
-        // AppBar defintion with style
+        // AppBar definition with style
         appBar: AppBar(
-          title: Text("Column App 01"),
-          backgroundColor: Colors.amberAccent,
-          titleTextStyle: TextStyle(
+          title: const Text("Column App 01"),
+
+          // Styling the text
+          titleTextStyle: const TextStyle(
             color: Colors.blueAccent,
-            fontWeight: FontWeight.w400
-          )
+            fontWeight: FontWeight.bold
+          ),
+          backgroundColor: Colors.amberAccent
         ),
 
         // Column definition
-        body: Column(
+        body: const Column(
+
+          // Define the main axis alignment
+          mainAxisAlignment: MainAxisAlignment.start,
 
           // Data is filled in the column.  Data is wrapped in Text widgets
           children: <Widget> [
             Text(
               "Reminder",
 
+              // Styling the reminder
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
 
-            Text("Upload slides on ulearn"),
+            // Styling the first record
+            Text(
+                "Upload slides on ulearn",
+              style: TextStyle(
+                color: Colors.white,
+                backgroundColor: Colors.blueAccent,
+              ),
+            ),
             Text("Upload lab exercises on ulearn"),
             Text("Update student attendance"),
           ],
